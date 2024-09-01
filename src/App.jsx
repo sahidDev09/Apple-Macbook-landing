a48c7f19a8dc1820a1af8169c9a30fcc91926f24
sahidDev09 <iamsahidofficial99@gmail.com>
2024-09-01 11:51:37 +0600

    add mac element 3d

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { Canvas } from "@react-three/fiber";
 import "./style.css";
-import { OrbitControls } from "@react-three/drei";
+import { Environment, ScrollControls } from "@react-three/drei";
 import MacLanding from "./components/MacLanding";
 
 function App() {
   return (
-    <Canvas>
-      <OrbitControls />
-      <MacLanding />
+    <Canvas camera={{ fov: 12, position: [0, -10, 220] }}>
+      <Environment
+        files={[
+          "https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr",
+        ]}
+      />
+      <ScrollControls>
+        <MacLanding />
+      </ScrollControls>
     </Canvas>
   );
 }
--- src/components/MacLanding.jsx
+++ src/components/MacLanding.jsx
@@ -1,11 +1,29 @@
+/* eslint-disable react/no-unknown-property */
+import { useGLTF, useScroll } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import * as THREE from "three";
 
 
 const MacLanding = () => {
-    return (
-        <div>
-            hello mac
-        </div>
-    );
+  let model = useGLTF("./mac.glb");
+  let meshes = {};
+
+  model.scene.traverse((e) => {
+    meshes[e.name] = e;
+  });
+
+  meshes.screen.rotation.x = THREE.MathUtils.degToRad(180);
+  let data = useScroll();
+
+  useFrame((state, delta) => {
+    meshes.screen.rotation.x = THREE.MathUtils.degToRad(180 - data.offset * 90);
+  });
+
+  return (
+    <group position={[0, -10, 20]}>
+      <primitive object={model.scene} />
+    </group>
+  );
 };
 
 export default MacLanding;
